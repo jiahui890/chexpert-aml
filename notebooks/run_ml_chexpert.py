c3c96057a6df8029e15cb472a0ec95411be3c438
Zhike Chen <zk.chen007@gmail.com>
2021-07-21 09:29:26 +0800

    change print to log and add duration

--- notebooks/run_ml_chexpert.py
+++ notebooks/run_ml_chexpert.py
@@ -18,8 +18,15 @@ from sklearn.naive_bayes import MultinomialNB
 from sklearn.pipeline import make_pipeline
 from sklearn.multioutput import MultiOutputClassifier
 from sklearn.metrics import roc_auc_score, roc_curve, f1_score, accuracy_score
+import logging
+from datetime import datetime
+
+logger = logging.getLogger(__file__)
 
 if __name__ == '__main__':
+    logging.basicConfig(level=logging.INFO,
+    format='%(asctime)s.%(msecs)03d %(levelname)s %(module)s: %(message)s',
+    datefmt='%Y-%m-%d %H:%M:%S')
 
     default_dir = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
     parser = argparse.ArgumentParser()
@@ -36,8 +43,8 @@ if __name__ == '__main__':
                         'Pleural Effusion'])
 
     args = parser.parse_args()
-    print(f'==============================================')
-    print('Loading dataset')
+    logger.info(f'==============================================')
+    logger.info('Loading dataset')
     base_path = args.path
     preprocessing_path = os.path.join(base_path,args.preprocessing)
     image_path = os.path.join(base_path, "data", "raw")
@@ -57,56 +64,62 @@ if __name__ == '__main__':
     if args.validsize is not None:
         valid_dataset = train_dataset.split(validsize=args.validsize)
     test_dataset = ImageDataset(label_csv_path=test_csv_path, image_path_base=image_path, transformations=preprocessing_config["transformations"])
-    print(f'train_dataset: {train_dataset}, {train_csv_path}')
-    print(f'test_dataset: {test_dataset}, {test_csv_path}')
-    print(f'==============================================')
+    logger.info(f'train_dataset: {train_dataset}, {train_csv_path}')
+    logger.info(f'test_dataset: {test_dataset}, {test_csv_path}')
+    logger.info(f'==============================================')
 
     num_batch = train_dataset._num_image // batch_size + bool(train_dataset._num_image % batch_size) #ceiling division
 
     # TODO: Setup your own model here
     if args.pca:
-        print(f'Setting up pca')
+        start_time = datetime.now()
+        logger.info(f'Setting up pca')
         pca = IncrementalPCA(n_components=50, whiten=True, batch_size=batch_size)
         for i, (x_features, x_image, y) in enumerate(train_dataset.batchloader(batch_size, return_labels)):
-            print(f'Training pca on batch {(i + 1)} out of {num_batch}')
+            logger.info(f'Training pca on batch {(i + 1)} out of {num_batch}')
             pca.partial_fit(x_image)
+        end_time = datetime.now()
+        logger.info('PCA fit duration: {}'.format(end_time - start_time))
 
-        print(f'Saving pca model .sav file... ')
+        logger.info(f'Saving pca model .sav file... ')
         f_datetime = dt.datetime.now().strftime('%H%M_%d%m%Y')
         pca_fname = os.path.join(model_path, f'{pca}_{batch_size}_{args.map}_{f_datetime}.sav')
         pickle.dump(pca, open(pca_fname, 'wb'))
 
     base_model = MultinomialNB()
-    print(f'model: {base_model}')
+    logger.info(f'model: {base_model}')
 
     if len(return_labels) > 1:
         model = MultiOutputClassifier(base_model)
-        print ("Extending to Multi Output Classifer")
+        logger.info ("Extending to Multi Output Classifer")
     else:
         model = base_model
 
     classes = np.array([[0, 1] for y in return_labels]).astype(np.float32)
+    start_time = datetime.now()
     for i, (x_features, x_image, y) in enumerate(train_dataset.batchloader(batch_size, return_labels)):
         if args.pca:
             x_image = MinMaxScaler().fit_transform(pca.transform(x_image))
         X = pd.concat([pd.DataFrame(x_features), pd.DataFrame(x_image)], axis=1)
-        print(f'Training model on batch {(i + 1)} out of {num_batch}')
+        logger.info(f'Training model on batch {(i + 1)} out of {num_batch}')
         model.partial_fit(X, y, classes=classes)
+    end_time = datetime.now()
+    logger.info('Model training duration: {}'.format(end_time - start_time))
 
-    print(f'Saving model .sav file... ')
+    logger.info(f'Saving model .sav file... ')
     f_datetime = dt.datetime.now().strftime('%H%M_%d%m%Y')
     model_fname = os.path.join(model_path, f'{args.file}_{batch_size}_{args.map}_{f_datetime}.sav')
     pickle.dump(model, open(model_fname, 'wb'))
-    print(f'Running model on test dataset...')
+    logger.info(f'Running model on test dataset...')
     x_features_test, x_image_test, y_test_multi = test_dataset.load(return_labels)
     if args.pca:
         x_image_test = MinMaxScaler().fit_transform(pca.transform(x_image_test))
     X_test = pd.concat([pd.DataFrame(x_features_test), pd.DataFrame(x_image_test)], axis=1)
     y_pred_multi = np.array(model.predict_proba(X_test))
     y_pred_labels = np.array(model.predict(X_test))
-    print(f'==============================================')
-    print(f'Verification results')
-    print(f'==============================================\n')
+    logger.info(f'==============================================')
+    logger.info(f'Verification results')
+    logger.info(f'==============================================\n')
 
     for idx, label in enumerate(return_labels):
         y_test = y_test_multi[label]
@@ -129,12 +142,12 @@ if __name__ == '__main__':
         fig.savefig(fig_fname, dpi=72)
         fig.clear()
         if args.pca:
-            print(f'PCA components: {pca.n_components}')
+            logger.info(f'PCA components: {pca.n_components}')
             f.write(f'PCA components: {pca.n_components}\n')
-        print(f'{label}')
-        print(f'roc_auc_score: {auc}')
-        print(f'accuracy: {accuracy}')
-        print(f'f1-score: {f1}')
+        logger.info(f'{label}')
+        logger.info(f'roc_auc_score: {auc}')
+        logger.info(f'accuracy: {accuracy}')
+        logger.info(f'f1-score: {f1}')
         f.write(f'{label}\n')
         f.write(f'roc_auc_score: {auc}\n')
         f.write(f'accuracy: {accuracy}\n')
