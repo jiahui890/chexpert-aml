79e681adf849600978a9efb97d4e6645231b6ccb
Wong Songhan <songhan89@gmail.com>
2021-07-25 15:26:27 +0800

    Added SGDClassifier with balanced model training dataset (only for full batch training).

--- src/models/sklearn_models.py
+++ src/models/sklearn_models.py
@@ -1,7 +1,11 @@
+import pickle
 from sklearn.linear_model import SGDClassifier
 from sklearn.naive_bayes import MultinomialNB
+from sklearn.pipeline import Pipeline
+
 
 models = {
     "MultinomialNB": MultinomialNB(),
     "SGDClassifier": SGDClassifier(loss='log'),
+    "SGDClassifier_Elastic": SGDClassifier(loss='huber', penalty='elasticnet', n_jobs=-1, class_weight='balanced')
 }
