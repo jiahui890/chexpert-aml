78e88f0bf1a1f9465c6d5b5cb8dc66a545753362
Wong Songhan <songhan89@gmail.com>
2021-07-27 01:38:37 +0800

    Added new `imgproc` obj under tf_read_image to call the relevant tf functions for image processing.

--- src/data/imgproc.py
+++ src/data/imgproc.py
@@ -28,25 +28,26 @@ def get_proc_class(module_name):
     if module_name == 'skimage':
         from .imgproc_skimage import SKImageProcessing
         return SKImageProcessing()
+    elif module_name == 'tfimage':
+        from .imgproc_skimage import TfImageProcessing
+        return TfImageProcessing()
     else:
         raise Exception(f'Unkown module name {module_name} for image process class')
 
-##TODO: merge processing image pipeline
-def tf_read_image(x_features, filename, label, channels=1, proc_module='skimage',
+def tf_read_image(x_features, filename, label, channels=1, proc_module='tfimage',
                   transformations=[
-                      ('resize', {'size': (320, 320)}),
-                      ('flatten', {})
+                      ('resize', {'size': (320, 320)},
+                       ('normalize', {}))
                   ]):
     image_string = tf.io.read_file(filename)
     imgproc = get_proc_class(proc_module)
 
     #Don't use tf.image.decode_image, or the output shape will be undefined
     image = tf.io.decode_jpeg(image_string, channels=channels)
-    #This will convert to float values in [0, 1]
-    image = tf.image.convert_image_dtype(image, tf.float32)
-    image = tf.image.resize(image, [320, 320])
+    image = imgproc.transform(image, transformations)
     image = tf.image.grayscale_to_rgb(image)
     image = tf.reshape(image, [1, 320, 320, 3])
     x_features = tf.reshape(x_features, [1,x_features.shape[0]])
     label = tf.reshape(label, [1, label.shape[0]])
     return (x_features, image), label
+
