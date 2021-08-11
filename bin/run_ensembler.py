f874bc284f81ac062214007981a84ca87f5077ec
Arnold Ng <arnold.ng.2020@mitb.smu.edu.sg>
2021-08-12 03:17:18 +0800

    ensembler update

--- bin/run_ensembler.py
+++ bin/run_ensembler.py
@@ -97,10 +97,10 @@ if __name__ == '__main__':
     tfds_test_keras = tfds_test_keras.batch(batch_size)
     tfds_test = tfds_test.batch(batch_size)
     
-    
     #prefetch
     tfds_test = tfds_test.prefetch(tf.data.AUTOTUNE)
     
+    #extract labels
     y_test_multi = []
     for x, test_label in tfds_test:
         for item in test_label:
@@ -114,13 +114,14 @@ if __name__ == '__main__':
     #create a list of models
     model_list = []
     
+    '''
     trad_ml_models = ['pca_gaussiannb_standard_full_500_Random_0502_31072021.sav',
                       'pca_gaussiannb_standard_full_500_U-one_0801_31072021.sav',
                       'pca_gaussiannb_standard_full_500_U-zero_0752_30072021.sav',
                       'RandomForest_10000_U-one_1138_31072021.sav',
                       'RandomForest_10000_U-zero_1059_31072021.sav',
-                      'RondomForest_10000_Random_1212_31072021.sav']
-    
+                      'RandomForest_10000_Random_1212_31072021.sav']
+ 
     cnn_models = ['DenseNet121_keras_5_16_Random_0506_30072021.sav',
                   'DenseNet121_keras_5_16_U-one_0402_30072021.sav',
                   'DenseNet121_keras_5_16_U-zero_0257_30072021.sav',
@@ -138,6 +139,11 @@ if __name__ == '__main__':
                   'ResNet152_new_3_16_Random_1445_30072021.sav',
                   'ResNet152_new_3_16_U-one_1210_30072021.sav',
                   'ResNet152_new_3_16_U-zero_0935_30072021.sav']
+    '''
+    trad_ml_models = ['pca_gaussiannb_standard_full_500_Random_0502_31072021.sav',
+                      'pca_gaussiannb_standard_full_500_U-one_0801_31072021.sav',
+                      'pca_gaussiannb_standard_full_500_U-zero_0752_30072021.sav']
+    cnn_models=['DenseNet121_keras_5_16_Random_0506_30072021.sav']
     
     #load PCA
     logger.info(f'Setting up pca')
@@ -147,16 +153,13 @@ if __name__ == '__main__':
         pca_f_path = os.path.join(models_path, 'IncrementalPCA_FullData_200_500_U-zero_0752_30072021.sav')
         with open(pca_f_path, 'rb') as file:
             pca = pickle.load(file)
-        logger.info(f'Pretrained pca {pca_f_path} .sav file loaded.')
-        logger.info(f'Pretrained pca: {pca}')
+        logger.info(f'Pretrained pca {pca_f_path} file loaded.')
     except:
-        logger.error(f'Pretrained pca {pca_f_path} .sav file cannot be loaded!')
+        logger.error(f'Pretrained pca {pca_f_path} file cannot be loaded!')
     #loop through the .sav files
     for model in sav_list:
         model_name = str.split(model,'.')[0]
-        #add model to list
-        model_list.append(model_name)
-        
+                
         #for trad ml
         if model in trad_ml_models:
             #load models
@@ -170,10 +173,9 @@ if __name__ == '__main__':
                 #load new model
                 with open(model_f_path, 'rb') as file:
                     model_used = pickle.load(file)
-                logger.info(f'Pretrained model {model_f_path} .sav file loaded.')
-                logger.info(f'Pretrained model: {model}')
+                logger.info(f'Pretrained model {model_f_path} file loaded.')
             except:
-                logger.error(f'Pretrained model {model_f_path} .sav file cannot be loaded!')
+                logger.error(f'Pretrained model {model_f_path} file cannot be loaded!')
             
             x_features_test, x_image_test, y_test_multi = test_dataset.load(return_labels)
             x_image_test = MinMaxScaler().fit_transform(pca.transform(x_image_test))
@@ -187,8 +189,11 @@ if __name__ == '__main__':
             
             #create new dictionary entry for the results
             y_pred_multi[model_name] = y_pred_helper
-    
-    #for cnn
+            
+            #add model to list, only if used
+            model_list.append(model_name)
+        
+        #for cnn
         elif model in cnn_models:
             try:
                 cnn_pretrained_path = os.path.join(models_path, model)
@@ -218,13 +223,16 @@ if __name__ == '__main__':
                                                               use_multiprocessing=True, 
                                                               workers=8)
             
+            #add model to list, only if used
+            model_list.append(model_name)
+            
     #export the results
-    y_pred_multi_export = pd.DataFrame(y_pred_multi)
-    
     raw_pred_fname = os.path.join(results_path,
-                                  f"raw_pred_{f_datetime}.csv")
+                                  f"raw_pred_{f_datetime}.pkl")
     
-    y_pred_multi_export.to_csv(raw_pred_fname)
+    pred_dict = open(raw_pred_fname, "wb")
+    pickle.dump(y_pred_multi, pred_dict)
+    pred_dict.close()
       
     #create a list of permutations
     #import itertools
@@ -251,27 +259,29 @@ if __name__ == '__main__':
     for combo in combinations:
         for i in range(args.num_models):
             if i == 0:
-                combined_pred = pd.DataFrame(y_pred_multi[model_name[combo[i]]])
+                combined_pred = pd.DataFrame(y_pred_multi[model_list[combo[i]]])
             else:
                 combined_pred = pd.concat([combined_pred,
-                                          y_pred_multi[model_name[combo[i]]]],
+                                          pd.DataFrame(y_pred_multi[model_list[combo[i]]])],
                                           axis = 1
                                           )
         #average the predicted logits
-        #create a groupby index
-        group_index = list(np.ravel(np.repeat([range(return_labels)],args.num_models, axis=0)))
-        combined_pred = combined_pred.groupby(group_index,
+        combined_pred = combined_pred.groupby(combined_pred.columns,
                                               axis=1).mean()
+        
+        #convert to np.array
+        combined_pred = np.array(combined_pred)
         
         #create ensemble name
         ensem_name = [model_list[combo[i]] for i in range(args.num_models)]
+        ensem_name = ';'.join(ensem_name)
         
         #add predictions to predicted dictionary
         y_pred_ensem[ensem_name] = combined_pred
             
         #calculate model stats
         for idx, label in enumerate(return_labels):
-            y_test = y_test_multi[:, idx]
+            y_test = y_test_multi[label]
             y_pred = combined_pred[:, idx]
             y_pred_label = np.zeros(shape=y_pred.shape)
             auc = roc_auc_score(y_true=y_test, y_score=y_pred)
@@ -288,16 +298,31 @@ if __name__ == '__main__':
             f1 = f1_score(y_true=y_test, y_pred=y_pred_label)
             f1_dummy = f1_score(y_true=y_test, y_pred=np.ones(shape=y_pred.shape))
             
-            #append results in the results list
-            results_ensem.append([ensem_name,
-                                  label,
-                                  auc,
-                                  cutoff,
-                                  accuracy_dummy,
-                                  accuracy,
-                                  f1_dummy,
-                                  f1])
+            #create one-hot encoded vector of models in ensemble
+            ensem_vect = np.repeat(0,len(model_list))
+            for i in range(args.num_models):
+                ensem_vect[combo[i]] = 1
             
+            ensem_vect = ensem_vect.tolist()
+            
+            #append results in the results list
+            results = [ensem_name,
+                       label,
+                       auc,
+                       cutoff,
+                       accuracy_dummy,
+                       accuracy,
+                       f1_dummy,
+                       f1]
+            
+            #join the results with the model one-hot vect
+            ensem_vect.extend(results)
+            
+            #append to results table
+            results_ensem.append(ensem_vect)
+            
+            #not necessary to print for each combo
+            '''
             #print out the results for the combinations
             logger.info(f'========================================')
             logger.info(f'{label}')
@@ -309,16 +334,87 @@ if __name__ == '__main__':
             logger.info(f'accuracy (dummy): {accuracy_dummy}')
             logger.info(f'f1-score: {f1}')
             logger.info(f'f1-score (dummy): {f1_dummy}')
-    
-    #export all the results
-    y_pred_ensem_export = pd.DataFrame(y_pred_ensem)    
-    results_ensem_export = pd.DataFrame(results_ensem)
-    
+            '''
+    #export the ensemble predictions
     pred_fname = os.path.join(results_path,
-                              f"pred_{f_datetime}.csv")
+                              f"pred_{f_datetime}.pkl")
+    
+    ensem_dict = open(pred_fname, "wb")
+    pickle.dump(y_pred_ensem, ensem_dict)
+    ensem_dict.close()
+
+    #export the results    
+    results_ensem = np.array(results_ensem)
+    results_ensem_colnames = model_list.copy()
+    results_ensem_colnames.extend(['ensem_name',
+                                   'label',
+                                   'auc',
+                                   'cutoff',
+                                   'accuracy_dummy',
+                                   'accuracy',
+                                   'f1_dummy',
+                                   'f1'])
+    
+    results_ensem_export = pd.DataFrame(results_ensem,
+                                        columns = results_ensem_colnames)
+       
     results_fname = os.path.join(results_path,
                                  f"results_{f_datetime}.csv")
     
-    y_pred_ensem_export.to_csv(pred_fname)
     results_ensem_export.to_csv(results_fname)
+    
+    #find the best performing model
+    results_ensem_export['auc'] = pd.to_numeric(results_ensem_export['auc'])
+    results_agg = results_ensem_export.groupby('ensem_name',
+                                               axis = 0)['auc'].mean()
+    
+    best_ensem = results_agg.idxmax
+    
+    print(best_ensem)
+    
+    #generate charts and results for best ensem
+    #calculate model stats
+    for idx, label in enumerate(return_labels):
+        y_test = y_test_multi[label]
+        y_pred = y_pred_ensem[best_ensem][:, idx]
+        y_pred_label = np.zeros(shape=y_pred.shape)
+        auc = roc_auc_score(y_true=y_test, y_score=y_pred)
+        fpr, tpr, thresholds = roc_curve(y_true=y_test, y_score=y_pred)
+        gmeans = np.sqrt(tpr * (1 - fpr))
+        # locate the index of the largest g-mean
+        ix = np.argmax(gmeans)
+        # assign the prob above threshold to be label 1
+        cutoff = thresholds[ix]
+        y_pred_label[y_pred >= cutoff] = 1
+        #calc accuracy and f1-score
+        accuracy_dummy = accuracy_score(y_true=y_test, y_pred=np.ones(shape=y_pred.shape))
+        accuracy = accuracy_score(y_true=y_test, y_pred=y_pred_label)
+        f1 = f1_score(y_true=y_test, y_pred=y_pred_label)
+        f1_dummy = f1_score(y_true=y_test, y_pred=np.ones(shape=y_pred.shape))
+        fig = plt.figure(figsize=(5, 5))
+        ax = fig.add_subplot()
+        ax.plot(fpr, tpr, label=f"AUC: %.3f " % (auc))
+        ax.legend()
+        ax.set_title(f"ROC curve for {label} {best_ensem}", fontsize=8)
+        ax.set_xlabel("False Positive Rate", fontsize=8)
+        ax.set_ylabel("True Positive Rate", fontsize=8)
+        fig_fname = os.path.join(results_path,
+                                 f"{best_ensem}_{label}_{f_datetime}.png")
+        text_fname = os.path.join(results_path,
+                                  f"{best_ensem}_{label}_{f_datetime}.txt")
+        f = open(text_fname, "w")
+        fig.savefig(fig_fname, dpi=200)
+        fig.clear()
+        
+        f.write(f'========================================\n')
+        f.write(f'{label}\n')
+        f.write(f'========================================\n')
+        f.write(f'Model: {best_ensem}\n')
+        f.write(f'roc_auc_score: {auc}\n')
+        f.write(f'best threshold: {cutoff}\n')
+        f.write(f'accuracy: {accuracy}\n')
+        f.write(f'accuracy (dummy): {accuracy_dummy}\n')
+        f.write(f'f1-score: {f1}\n')
+        f.write(f'f1-score (dummy): {f1_dummy}\n')
+        f.close()
     
